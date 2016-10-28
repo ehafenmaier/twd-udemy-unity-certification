e8e685f238ba4aaf752ec565b95236709f9eaf2c
Erich Hafenmaier <ehafenmaier@gmail.com>
2016-10-27 23:52:33 -0600

    Physics Lessons
    Project for the physics lessons.

---
+++ UdemyCert_Physics/Assets/Physics Asset Package/Hoop/GoalScoring.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScoring : MonoBehaviour
+{
+    public Highlighter highlighter;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("Goal Scored..!");
+
+        if (highlighter)
+        {
+            highlighter.Highlight();
+        }
+    }
+}
