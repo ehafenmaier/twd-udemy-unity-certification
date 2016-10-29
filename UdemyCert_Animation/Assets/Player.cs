8b92ea2cce072d39ed19e5b181e594a486d4b0c9
Erich Hafenmaier <ehafenmaier@gmail.com>
2016-10-29 14:53:49 -0600

    Animation Lessons
    Committing the project from the animation lessons.

---
+++ UdemyCert_Animation/Assets/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour 
+{
+    Animator playerAnimator;
+    Rigidbody rb;
+    public float upJumpSpeed = 5;
+    public float forwardJumpSpeed = 1;
+
+	// Use this for initialization
+	void Awake () 
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerAnimator.applyRootMotion = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () 
+    {
+        var v = Input.GetAxis("Vertical");
+        var h = Input.GetAxis("Horizontal");
+
+
+        playerAnimator.SetFloat("ForwardSpeed", v);
+        playerAnimator.SetFloat("TurnSpeed", h);
+
+        if (Input.GetButtonDown("Duck"))
+        {
+            playerAnimator.SetTrigger("Crouch");
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerAnimator.SetTrigger("JumpUp");
+            rb.velocity += Vector3.up * upJumpSpeed + transform.forward * forwardJumpSpeed;
+        }
+
+        bool grounded = Mathf.Abs(Vector3.Dot(rb.velocity, Vector3.up)) < 0.01;
+        playerAnimator.applyRootMotion = grounded;
+        playerAnimator.SetBool("IsGrounded", grounded);
+    }
+}
